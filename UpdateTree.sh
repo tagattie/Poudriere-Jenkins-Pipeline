c55edddeaff6433e4ca2a29c8a0c953ffceb6524
Hiroki Tagato <tagattie@gmail.com>
2017-09-01 15:47:11 +0900

    Show more information on ports tree update.

--- UpdateTree.sh
+++ UpdateTree.sh
@@ -5,4 +5,4 @@ export PATH=/bin:/usr/bin:/usr/local/bin
 . ${WORKSPACE}/local.conf
 
 # Update the poudriere default ports tree
-sudo poudriere ports -u -p ${PORTSTREE}
+sudo poudriere ports -v -u -p ${PORTSTREE}
