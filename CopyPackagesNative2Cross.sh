93d32d26d891f456f48203065c430d3e814b7594
Hiroki Tagato <tagattie@gmail.com>
2017-11-01 16:38:38 +0900

    Sync back native to cross-built directory only when native directory timestamp is later than cross one.

--- CopyPackagesNative2Cross.sh
+++ CopyPackagesNative2Cross.sh
@@ -12,7 +12,21 @@ PORTSTREE=${3}
 NATIVEPKGDIR=${PKGBASEDIR}/${JAILNAMEPREFIX}${ARCH}-${PORTSTREE}
 CROSSPKGDIR=${PKGBASEDIR}/${JAILNAMEPREFIX}${ARCH}${CROSSSUFFIX}-${PORTSTREE}
 
-# Simply sync contents of native and cross package directories
+# Find the latest .real-XXXX directory of cross-built packages
+LATESTCROSSREALDIR=$(find ${CROSSPKGDIR}/ -type d -depth 1 -print | \
+    awk -F'/' '{print $NF}' | \
+    sort -nr | \
+    head -n 1)
+# Find the latest .real-XXXX directory of native-built packages
+LATESTNATIVEREALDIR=$(find ${NATIVEPKGDIR}/ -type d -depth 1 -print | \
+    awk -F'/' '{print $NF}' | \
+    sort -nr | \
+    head -n 1)
+echo "Latest cross-built .real-XXXX directory is ${LATESTCROSSREALDIR}."
+echo "Latest native-built .real-XXXX directory is ${LATESTNATIVEREALDIR}."
+
+# Sync contents of native and cross package directories
+# when native building succeeded
 # (Sync direction is cross pkg dir -> native pkg dir.)
 if [ "${DRYRUNCOPY}" == "true" ]; then
     DRYRUN_FLAG="-n"
@@ -20,7 +34,22 @@ fi
 if  [ "${VERBOSECOPY}" == "true" ]; then
     VERBOSE_FLAG="-v"
 fi
-sudo rsync ${DRYRUN_FLAG} ${VERBOSE_FLAG} \
-    -a --info=STATS3 --delete --stats \
-    ${NATIVEPKGDIR}/ ${CROSSPKGDIR}
-# sudo cpdup ${DRYRUN_FLAG} -i0 -x -I ${NATIVEPKGDIR} ${CROSSPKGDIR}
+REALDIRSUBSTR="real"
+# Check if the latest real dirs are really .real_XXXX and not .building
+if [ "${LATESTCROSSREALDIR#*$REALDIRSUBSTR*}" != "${LATESTCROSSREALDIR}" ] && \
+    ["${LATESTNATIVEREALDIR#*$REALDIRSUBSTR*}" != "${LATESTNATIVEREALDIR}" ]; then
+    CROSSREALDIREPOCH=$(echo ${LATESTCROSSREALDIR} | awk -F'_' '{print $NF}')
+    NATIVEREALDIREPOCH=$(echo ${LATESTNATIVEREALDIR} | awk -F'_' '{print $NF}')
+    if [ ${NATIVEREALDIREPOCH} -gt ${CROSSREALDIREPOCH} ]; then
+        # Sync only when some new packages have been commited in the latest
+        # native-built real directory
+        echo "Syncing native-built package directory with cross-built one."
+        sudo rsync ${DRYRUN_FLAG} ${VERBOSE_FLAG} \
+            -a --info=STATS3 --delete --stats \
+            ${NATIVEPKGDIR}/ ${CROSSPKGDIR}
+        # sudo cpdup ${DRYRUN_FLAG} -i0 -x -I ${NATIVEPKGDIR} ${CROSSPKGDIR}
+    else
+        echo "${LATESTNATIVEREALDIR} is not newer than ${LATESTCROSSREALDIR}."
+        echo "Skipping copying native -> cross package directory."
+    fi
+fi
